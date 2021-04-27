651ed81ef3762660bf4313b86147d66c1b2d5b37
contactcodeapp <ddougherty@contactcodeapp.com>
2021-04-26 21:21:51 -0400

    completed unit tests

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,61 +6,73 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(3, 4, 5, 12)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var a = new UnitTestMethods();
 
             //Act
+            var actual = a.Add(num1, num2, num3);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(8, 6, 2)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var s = new UnitTestMethods();
 
             //Act
+            var actaul = s.Subtract(minuend, subtrhend);
 
             //Assert
-
+            Assert.Equal(expected, actaul);
+             
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(3, 7, 21)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var m = new UnitTestMethods();
 
             //Act
+            var actual = m.Multiply(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(72, 8, 9)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var d = new UnitTestMethods();
 
             //Act
+            var actual = d.Divide(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void YourMethodName()
         {
             //Arrange
+            var f = new UnitTestMethods();
 
             //Act
+            var actual = f.Fact1();
 
             //Assert
+            Assert.Equal("Surf's Up!", actual);
 
         }
 
@@ -68,10 +80,13 @@ namespace UnitTestingExercise.Tests
         public void YourMethodName2()
         {
             //Arrange
+            var f2 = new UnitTestMethods();
 
             //Act
+            var actual = f2.Fact2();
 
             //Assert
+            Assert.Equal("Kook", actual);
         }
     }
 }
